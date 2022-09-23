549ea62a2397648e59eba86539a4b450f1a39f85
KazeDao <107550463+KazeDao@users.noreply.github.com>
2022-09-23 03:17:07 +0300

    Add files via upload

--- NPaper/NewsPortal/views.py
+++ NPaper/NewsPortal/views.py
@@ -9,10 +9,6 @@ class NewsList(ListView):
     template_name = 'news.html'
     context_object_name = 'news'
 
-    #def get_context_data(self, **kwargs):
-        #context = super().get_context_data(**kwargs)
-        #return context
-
 
 class NewsDetail(DetailView):
     model = Post
